ad8d57e43afdd7002536eae096b32049c8db3c73
Захарчук Тимофей Викторович <t.zaharchuk@AVK-Company.local>
2022-09-23 12:11:52 +0300

    update

--- setup.bat
+++ setup.bat
@@ -4,9 +4,9 @@
 set /p RSYSLOG_SERVER="ENTER IP RSYSLOG SERVER:: "
 
 
-C:\wget.exe -OC:\Windows\System32 https://github.com/linuxbuh/eventlog-to-syslog/raw/main/evtsys.exe
+copy %~dp0\evtsys.exe C:\Windows\System32\evtsys.exe
 @pause 10
-evtsys.exe -i -h %RSYSLOG_SERVER%
+C:\Windows\System32\evtsys.exe -i -h %RSYSLOG_SERVER%
 @pause 10
 net start evtsys
 
